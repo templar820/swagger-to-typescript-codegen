f54d428523a10a29f0d8457c1f20e8d09eef3a4c
Кузнецов Иван Дмитриевич <41190483+templar820@users.noreply.github.com>
2022-02-21 19:55:15 +0300

    макет

---
+++ src/Api.service.ts
@@ -0,0 +1,183 @@
+import { pathList, PathListInterface } from '../api/api';
+import jsonConfig from './config.json';
+
+interface ApiServiceConfig {
+    onBeforeRequest?: (path: string, parameters: any, type: string) => void;
+    onAfterReceivingResponse?: (res: Response) => void;
+    responseHandler:(res: Response) => any;
+    endpoint: string;
+    headers: HeadersInit;
+}
+
+type requestOptionsType = {
+    method: string;
+    headers: any;
+    body?: string | FormData;
+}
+
+class ApiService {
+    requests: PathListInterface;
+
+    config: ApiServiceConfig;
+
+    constructor(config: ApiServiceConfig = {} as ApiServiceConfig) {
+        // @ts-ignore
+        this.requests = pathList;
+
+        this.config = config;
+        this.initMethods();
+    }
+
+    private initMethods() {
+        const getNestedMethod = obj => {
+            if (!this.isObject(obj)) return;
+            for (const [key, value] of Object.entries(obj)) {
+                if (jsonConfig.restApiMethodsArray.includes(key)) {
+                    const requestData = JSON.parse(JSON.stringify(obj[key]));
+                    const headers = this.getHeaders();
+                    obj[key] = ((...args) => {
+                        if (Array.isArray(requestData)) {
+                            const request = requestData.find(options => {
+                                const [body, parameters] = this.getBodyAndParamsFromArgs(options, args);
+                                const pathParams = options.parameters?.reduce((acc, current) => {
+                                    if (current.in === 'path' || current.in === 'query') {
+                                        acc.push(current);
+                                    }
+                                    return acc;
+                                }, []);
+                                if (!Array.isArray(pathParams) && !parameters?.path && !parameters?.query) return true;
+                                try {
+                                    // Тут внимательно сверяем что в апи, и что в функции прилетело ну т.е. чекаем на совпадение
+                                    return pathParams.some(el => ((parameters?.path && parameters.path[el.name]) && el.required)
+                                        || ((el.in === 'query')
+                                            && (!el.required || (parameters?.query && (parameters?.query[el.name])))
+                                        ));
+                                } catch (e) {
+                                    return false;
+                                }
+                            });
+                            return this.createRequest(args, key, headers, request);
+                        }
+                        return this.createRequest(args, key, headers, requestData);
+                    });
+                }
+                getNestedMethod(value);
+            }
+        };
+
+        getNestedMethod(this.requests);
+    }
+
+    private createRequest(args, key, headers, requestData) {
+        const [body, parameters, options] = this.getBodyAndParamsFromArgs(requestData, args);
+
+        const path = this.getPathWithParams(
+            parameters,
+            requestData
+        );
+        return this.fetch(path, body, key, headers, options);
+    }
+
+    private getBodyAndParamsFromArgs(requestData, args) {
+        const body = requestData?.body ? args[0] : null;
+        let parameters = null;
+        let options;
+        if (body) {
+            if (args[1] && ('path' in args[1] || 'query' in args[1])) {
+                options = args[2];
+                parameters = args[1];
+            } else {
+                options = args[1];
+            }
+        } else {
+            parameters = args[0];
+            options = args[1];
+        }
+        return [body, parameters, options];
+    }
+
+    private getHeaders() {
+        const headers = this.config.headers || {};
+        headers['Content-Type'] = 'application/json';
+        return headers;
+    }
+
+    private getPathWithParams(parameters, requestData) {
+        let { path } = requestData;
+        let firstQueryFlag = true;
+        if (parameters) {
+            requestData.parameters?.forEach(param => {
+                if (requestData.path.includes(param.name) && param.in === 'path') {
+                    path = path.replace(`{${param.name}}`, parameters.path[param.name]);
+                }
+                if (param.in === 'query') {
+                    if (parameters.query[param.name]) {
+                        if (firstQueryFlag) {
+                            path += '?';
+                            firstQueryFlag = false;
+                        } else {
+                            path += '&';
+                        }
+                        path += `${ param.name }=${ parameters.query[param.name]}`;
+                    }
+                }
+            });
+        }
+        return path;
+    }
+
+    private isObject(item) {
+        return (item && typeof item === 'object' && !Array.isArray(item));
+    }
+
+    private async fetch(alias, parameters, type = 'POST', headers, requestOptions) {
+        const options: requestOptionsType = {
+            method: type.toUpperCase(),
+            headers,
+        };
+        if (parameters) {
+            if (requestOptions?.toFile) {
+                const data = new FormData();
+                for (const [key, value] of Object.entries(parameters)) {
+                    data.append(key, value);
+                }
+                options.body = data;
+                delete options.headers['Content-Type'];
+            } else {
+                options.body = JSON.stringify(parameters);
+            }
+        }
+
+        if (this.config.onBeforeRequest) {
+            this.config.onBeforeRequest(alias, parameters, type);
+        }
+
+        try {
+            const res = await fetch(this.config.endpoint + alias, options);
+            return this.checkResponse(res);
+        } catch (err) {
+            return this.checkResponse(err);
+        }
+    }
+
+    private async checkResponse(res) {
+        let response;
+        if (this.config.onAfterReceivingResponse) {
+            this.config.onAfterReceivingResponse(res);
+        }
+
+        if (this.config.responseHandler) {
+            return this.config.responseHandler(res);
+        } else {
+            try {
+                response = await res.json();
+            } catch (e) {
+                throw new Error('internal server Error');
+            }
+            return response;
+        }
+
+    }
+}
+
+export default ApiService;
---
+++ src/codegen/index.mjs
@@ -0,0 +1,162 @@
+import path from 'path';
+import fs from 'fs';
+import { execSync } from "child_process";
+import requestConfig from "../config.json"
+import { generateName, getBodyType, getJSONSwagger, getMethodName, getParameters, getParametersType, getRequestHeadersType, getRequestMethods, getResponse, getSegmentName, mergeDeep, setCamelCaseKeys } from "./utils.mjs";
+
+if (typeof String.prototype.replaceAll === 'undefined') {
+    String.prototype.replaceAll = function (match, replace) {
+        return this.replace(new RegExp(match, 'g'), () => replace);
+    };
+}
+
+const fileName = './src/api/api.ts';
+const filePath = path.resolve(fileName);
+
+
+
+export default async function CreateApiService(config) {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    execSync(`npx openapi-typescript ${config.swaggerEndpoint} --output ${filePath}`);
+    addPaths(config);
+}
+
+
+let overloadType = "";
+
+const addPaths = async (config) => {
+    const json = await getJSONSwagger(config.swaggerEndpoint);
+
+    const pathListInterface = swaggerParamsList(json, config, (restApiTag, jsonItem, segmentName, methodName, item, acc) => {
+        const bodyType = getBodyType(jsonItem, restApiTag);
+        const parametersType = getParametersType(jsonItem, restApiTag);
+
+        let functionType = '';
+        const operations = `operations['${jsonItem[restApiTag].operationId}']`;
+        const answer = `Promise<${getResponse(jsonItem, restApiTag, operations)}>`
+
+
+        if (bodyType && !parametersType) {
+            functionType = `"(body: ${operations + bodyType}) => ${answer}"`;
+        }
+        if (!bodyType && parametersType) {
+            let isRequired = jsonItem[restApiTag].parameters.every(el => el.required);
+            functionType = `"(parameters ${isRequired ? "": "?"}: ${operations + parametersType} ) => ${answer}"`;
+        }
+        if (bodyType && parametersType) {
+            functionType = `"(body: ${operations + bodyType}, parameters : ${operations + parametersType} ) => ${answer}"`;
+        }
+        if (!bodyType && !parametersType) {
+            functionType = `"() => ${answer}"`;
+        }
+        const name = generateName();
+        if (segmentName === methodName && acc[segmentName] && acc[segmentName][restApiTag]) {
+            let oldType = acc[segmentName][restApiTag];
+            overloadType += oldType.replace("(", `function ${name} (`).replace("=>", ":") + "\n"
+            overloadType += functionType.replace("(", `function ${name} (`).replace("=>", ":") + "\n"
+            functionType = `typeof ${name}`
+        } else {
+            if (acc[segmentName] && acc[segmentName][methodName] && acc[segmentName][methodName][restApiTag]){
+                let oldType = acc[segmentName][methodName][restApiTag];
+                overloadType += oldType.replace("(", `function ${name} (`).replace("=>", ":") + "\n"
+                overloadType += functionType.replace("(", `function ${name} (`).replace("=>", ":") + "\n"
+                functionType = `typeof ${name}`
+            }
+        }
+        return functionType;
+    })
+
+
+
+    const pathList = swaggerParamsList(json, config,(restApiTag, jsonItem, segmentName, methodName, item, acc) => {
+        const operationId = jsonItem[restApiTag].operationId
+        const bodyType = getBodyType(jsonItem, restApiTag);
+        const newData = {
+            id: operationId,
+            body: !!bodyType,
+            parameters: getParameters(jsonItem, restApiTag),
+            path: item,
+        }
+
+
+        if (segmentName === methodName && acc[segmentName] && acc[segmentName][restApiTag]) {
+            let oldData = acc[segmentName][restApiTag];
+            return [newData, oldData]
+        } else {
+            if (acc[segmentName] && acc[segmentName][methodName] && acc[segmentName][methodName][restApiTag]){
+                let oldData = acc[segmentName][methodName][restApiTag];
+                return [newData, Array.isArray(oldData)? [...oldData]: oldData]
+            }
+        }
+        return newData
+    })
+
+
+    setCamelCaseKeys(pathListInterface);
+    setCamelCaseKeys(pathList);
+    generateResult(pathListInterface, pathList)
+};
+
+
+
+function swaggerParamsList(json, config, callback){
+    const pathsArray = Object.keys(json.paths);
+    return pathsArray.reduce((acc, item) => {
+        const jsonItem = json.paths[item];
+        const methods = getRequestMethods(jsonItem, requestConfig.restApiMethodsArray);
+        if (!methods.length) return acc;
+        const arr = item.split('/').filter(el => !!el);
+        const segmentName = getSegmentName(arr, config.prefix);
+        const methodName = getMethodName(arr, config.prefix);
+
+        if (!segmentName || !methodName) return acc;
+        const oldMethods = acc[segmentName] || {};
+        const newMethod = {};
+        const path = [...new Set([segmentName, methodName])]
+
+        methods.forEach(restApiTag => {
+            const result = callback(restApiTag, jsonItem, segmentName, methodName, item, acc);
+            if (segmentName === methodName) {
+                newMethod[restApiTag] = result
+            } else {
+                mergeDeep(newMethod, {
+                    [methodName]: {
+                        [restApiTag]: result
+                    }
+                });
+            }
+        });
+        return {
+            ...acc,
+            [segmentName]:  mergeDeep(oldMethods, newMethod),
+        };
+    }, {});
+}
+
+index();
+
+
+function generateResult(pathListInterface, pathList){
+    const apiFile = fs.readFileSync(filePath, { encoding: 'utf8' });
+
+    const result = `
+  ${overloadType.replaceAll('"', '')}
+  
+  export interface PathListInterface ${JSON.stringify(
+        pathListInterface,
+        null,
+        2
+    )
+        .replaceAll('"', '')
+        .replaceAll(',\n', ';\n')
+        .replaceAll(/\\/g, '')
+    }\n
+  export const pathList =  ${
+        JSON.stringify(
+            pathList,
+            null,
+            2
+        )}
+  ${apiFile}`;
+    fs.writeFileSync(filePath, result);
+}
