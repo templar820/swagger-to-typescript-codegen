408e35cd77cb98855b7f0808a04dcbe5fbcff54c
templar820 <ttemplar820@gmail.com>
2022-02-23 16:30:18 +0300

    prepare to publish

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import CreateApiService from "./src/codegen";
+import ApiService from "./api/Api.service";
+import * as ISwagger from "./api/api"
 
 
+// CreateApiService({
+//     outputPath: "./api",
+//     swaggerEndpoint: "http://51.250.9.175/api/schema/?format=yaml",
+// })
 
 
-CreateApiService({
-    outputPath: "./api",
-    swaggerEndpoint: "http://51.250.9.175/api/schema/?format=yaml",
-})
+export {
+  CreateApiService,
+  ApiService,
+  ISwagger
+}
